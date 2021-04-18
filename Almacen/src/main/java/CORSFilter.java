49d31c54eeeeff555f769c0cab6f07546ae3718e
Fragore <alexisfonzos_@hotmail.com>
2021-04-18 13:59:56 -0300

    Adding more CORS

--- Almacen/src/main/java/CORSFilter.java
+++ Almacen/src/main/java/CORSFilter.java
@@ -8,23 +8,35 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CORSFilter implements Filter {
 
-	public CORSFilter() { }
+	public CORSFilter() {
+	}
 
-	public void init(FilterConfig fConfig) throws ServletException { }
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
 
-	public void destroy() {	}
+	public void destroy() {
+	}
 
-	public void doFilter(
-		ServletRequest request, ServletResponse response, 
-		FilterChain chain) throws IOException, ServletException {
-
-		((HttpServletResponse)response).addHeader(
-			"Access-Control-Allow-Origin", "*"
-		);
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		
+		((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
+		((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
+		
+		HttpServletResponse resp = (HttpServletResponse) response;
+		 
+        // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
+        if (((HttpServletRequest)request).getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+		
 		chain.doFilter(request, response);
 	}
 }
