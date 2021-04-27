5ae28613d614d5a0d5ea5bedf886b4f399079803
Daiko <daiko_011@hotmail.com>
2021-04-27 09:37:15 -0300

    el titulo anterior se referia a este commit

--- Almacen/src/main/java/Almacen/persistence/LlaveDB.java
+++ Almacen/src/main/java/Almacen/persistence/LlaveDB.java
@@ -1,5 +1,6 @@
 package main.java.Almacen.persistence;
 
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 
 import main.java.Almacen.model.Llave;
@@ -13,11 +14,12 @@ public class LlaveDB {
 		try {
 			sess = HibernateUtils.openSession();
 			llave = sess.get(Llave.class, id);
+			Hibernate.initialize(llave.getUbicacion());
+			return llave;
 
 		} finally {
 			sess.close();
 		}
-		return llave;
 	}
 
 }
